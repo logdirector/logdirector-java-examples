2d3d727322a2914cdacc3ef95664ebf7685bb412
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2013-01-25 21:27:01 +0100

    Copyright stuff

--- src/CopyJobExample.java
+++ src/CopyJobExample.java
@@ -5,10 +5,17 @@ import java.io.FileNotFoundException;
 
 /**
  * Created with IntelliJ IDEA.
- * User: sasch_000
+ * User: Sascha Ißbrücker
  * Date: 25.01.13
  * Time: 09:02
+ *
  * Just a small example that simulates a job that copies some files from one folder to another.
+ *
+ * Copyright (c) 2013 simonigence gmbh
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 public class CopyJobExample {
 
